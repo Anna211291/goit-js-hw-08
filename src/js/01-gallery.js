3f864bd2c3cc62503def4f16166617e4d05dd9cf
Anna211291 <Anjutka777@gmail.com>
2023-08-19 01:17:46 -0400

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("") 
+}
+
+gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+let galleryModal = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+galleryModal.on('show.simplelightbox', function () {
+
+});
