ff4c5ed412ad4e323c2fae2645a11f8ac6cec3f0
Anna211291 <Anjutka777@gmail.com>
2023-08-20 16:56:05 -0400

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector(".feedback-form");
+const LOCALS_KEY = "feedback-form-state"; 
 
+form.addEventListener("input", throttle(saveInput, 500));
+form.addEventListener("submit", submitForm)
+
+let inputs = JSON.parse(localStorage.getItem(LOCALS_KEY)) || {};
+
+const { email, message } = form.elements;
+
+email.value = inputs.email || "";
+message.value = inputs.message || "";
+
+function saveInput(evt) {
+    evt.preventDefault();
+    inputs[evt.target.name] = evt.target.value;
+localStorage.setItem(LOCALS_KEY, JSON.stringify(inputs));
+};
+
+function submitForm(evt) {
+    evt.preventDefault();
+    console.log(inputs);
+    localStorage.removeItem(LOCALS_KEY);
+};
